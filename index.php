7778be9e34ad5315c5e58f4adac478f2abd74e38
bhsingh1 <64067363+bhsingh1@users.noreply.github.com>
2020-05-11 12:12:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
   print "I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
+  
   echo "Welcome to Red Dias for sizing your applications on OpenShift";
   echo "<br>To test the database, hit the dbtest.php URL";
 
