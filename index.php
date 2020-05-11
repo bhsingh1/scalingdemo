e326fc013169f94c32627280723507381edb290c
bhsingh1 <64067363+bhsingh1@users.noreply.github.com>
2020-05-11 15:29:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
-<?php
-  print "I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
+<?php  
+  echo "Welcome to Red Dias for Sizing your Applications on OpenShift";
+  print "We are running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
   
-  echo "Welcome to Red Dias for sizing your applications on OpenShift";
   echo "<br>To test the database, hit the dbtest.php URL";
 
-  echo "<br><br>This assumes that you have the correct env variables set";
-  echo "<BR><BR>The environment variables required are databaseuser, databasepassword, and databasenmae.";
+  echo "<BR><BR>The environment variables required are databaseuser, databasepassword, and databasename.";
