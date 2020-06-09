9d019a83463013ecec812f4e9823d60d005532aa
bhsingh1 <64067363+bhsingh1@users.noreply.github.com>
2020-06-10 01:36:49 +0530

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -1,14 +1,15 @@
 <?php
-#$dbhost = getenv("MYSQL_SERVICE_HOST");
-#$dbport = getenv("MYSQL_SERVICE_PORT");
-$dbuser = getenv("databaseuser");
-$dbpwd = getenv("databasepassword");
-$dbname = getenv("databasename");
+#$dbhost = getenv("POSTGRESQL_SERVICE_HOST");
+#$dbport = getenv("POSTGRESQL_SERVICE_PORT");
+#$dbuser = getenv("databaseuser");
+#$dbpwd = getenv("databasepassword");
+
+#$dbname = getenv("databasename");
 
 #error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE);
 #$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 #$connection=mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname);
-if (!$connection = pg_connect ("host=localhost, $dbuser, $dbpwd, $dbname")) {
+if (!$connection = pg_connect ("host=localhost dbname=site user=postgres password=root")) {
     $error = error_get_last();
     echo "Connection failed. Error was: ". $error['message']. "\n";
 } else {
