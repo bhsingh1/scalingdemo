9fa935eccc34a28620d5511b6272a8058f3d1599
bhsingh1 <64067363+bhsingh1@users.noreply.github.com>
2020-06-10 02:39:26 +0530

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -5,7 +5,7 @@ $dbuser = getenv("databaseuser");
 $dbpwd = getenv("databasepassword");
 $dbname = getenv("databasename");
 
-error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE);
+error_reporting(E_WARNING);
 $connection=pg_connect($dbhost, $dbuser, $dbpwd, $dbname1);
 if ($connection->connect_errno) {
     printf("Connect failed: %s\n");
