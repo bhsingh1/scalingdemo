eb880cc5c180742e984cc563446937085d2ca4db
bhsingh1 <64067363+bhsingh1@users.noreply.github.com>
2020-06-10 01:21:11 +0530

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -5,13 +5,15 @@ $dbuser = getenv("databaseuser");
 $dbpwd = getenv("databasepassword");
 $dbname = getenv("databasename");
 
-error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE);
-$connection=mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname);
+#error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE);
+$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+#$connection=mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
+    printf("Connect failed: %s\n", $postgresqli->connect_error);
     exit();
 } else {
     printf("Connected to the database");
 }
+$connection->close();
 #$connection->close();
 ?>
