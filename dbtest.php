5cb127be955fb36c334c6697c4e25d2ee422c60b
bhsingh1 <64067363+bhsingh1@users.noreply.github.com>
2020-06-10 02:12:39 +0530

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -5,16 +5,24 @@ $dbuser = getenv("databaseuser");
 $dbpwd = getenv("databasepassword");
 $dbname = getenv("databasename");
 
+$db = pg_connect( "$dbhost, $dbuser, $dbpwd, $dbname"  );
+if(!$db) {
+   echo "Error : Unable to open database\n";
+} else {
+   echo "Opened database successfully\n";
+}
+?>
+
 #error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE);
 #$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 #$connection=mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname);
-if (!$connection=pg_pconnect("$dbhost, $dbuser, $dbpwd, $dbname")) {
+#if (!$connection=pg_pconnect("$dbhost, $dbuser, $dbpwd, $dbname")) {
 #if (!$connection = pg_connect ("host=localhost dbname=sampledb user=postgres password=root")) {
-    $error = error_get_last();
-    echo "Connection failed. Error was: ". $error['message']. "\n";
-} else {
-    echo "Connection succesful.\n";
-}
+    #$error = error_get_last();
+    #echo "Connection failed. Error was: ". $error['message']. "\n";
+#} else {
+    #echo "Connection succesful.\n";
+#}
 
 #if ($connection->connect_errno) {
     #printf("Connect failed: %s\n", $postgresqli->connect_error);
