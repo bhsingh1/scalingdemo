a6a0e1ae6b2624879efd358c1c2f27fd335c6217
bhsingh1 <64067363+bhsingh1@users.noreply.github.com>
2020-06-10 02:25:12 +0530

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -5,31 +5,13 @@ $dbuser = getenv("databaseuser");
 $dbpwd = getenv("databasepassword");
 $dbname = getenv("databasename");
 
-$db = pg_pconnect( "$dbhost, $dbuser, $dbpwd, $dbname"  );
-if(!$db) {
-   echo "Error : Unable to open database\n";
+error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE);
+$connection=pg_connect($dbhost, $dbuser, $dbpwd, $dbname);
+if ($connection->connect_errno) {
+    printf("Connect failed: %s\n");
+    exit();
 } else {
-   echo "Opened database successfully\n";
+    printf("Connected to the database");
 }
-?>
-
-#error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE);
-#$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
-#$connection=mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname);
-#if (!$connection=pg_pconnect("$dbhost, $dbuser, $dbpwd, $dbname")) {
-#if (!$connection = pg_connect ("host=localhost dbname=sampledb user=postgres password=root")) {
-    #$error = error_get_last();
-    #echo "Connection failed. Error was: ". $error['message']. "\n";
-#} else {
-    #echo "Connection succesful.\n";
-#}
-
-#if ($connection->connect_errno) {
-    #printf("Connect failed: %s\n", $postgresqli->connect_error);
-    #exit();
-#} else {
-    #printf("Connected to the database");
-#}
-#$connection->close();
 #$connection->close();
 ?>
